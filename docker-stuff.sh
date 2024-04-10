41bcc81870c6614337240027abe3ca5eb7b5a964
Matt Felton <matt.felton@feltonma.com>
2024-04-10 15:38:25 -0400

    modified - updated to 1.0
    * Added functionality to display token usage for prompt
      response, and total
    * Added support for receiving openai endpoint from upstream
      APIM to allow APIM to load balance
    * Added additional headers for response time from this solution and
      Azure OpenAI instance
    * Modified logs to write to json instead of custom text entry
    * Modified lua to filter out authorization tokens when logging to file
    * Modified parsing logic and renamed python helper to processor.py

--- docker-stuff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker stop openaiproxy
-docker container rm openaiproxy
-docker build -t openaiproxy .
-docker run -p 8888:80 -d --env-file .env --name openaiproxy openaiproxy
-docker logs -f openaiproxy
--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 envsubst '$DNS_SERVER' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
-nohup python /opt/loghelper/openai.py 2>&1 &
+nohup python /opt/loghelper/processor.py 2>&1 &
 /usr/local/openresty/bin/openresty -g "daemon off;"
