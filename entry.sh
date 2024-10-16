8e22bb8ee9a8301121cf5891bf3a5b63c7cedb37
mattfeltonma <matt.felton@feltonma.com>
2024-10-16 18:59:27 +0000

    modified - simplified entry.sh
    * Simplified entry.sh to replace both variables in
        the same line.

--- entry.sh
+++ entry.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-envsubst '$DNS_SERVER' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-envsubst '$GEN_AI_GATEWAY_FQDN' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$DNS_SERVER $GEN_AI_GATEWAY_FQDN' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 nohup python /opt/loghelper/processor.py 2>&1 &
 /usr/local/openresty/bin/openresty -g "daemon off;"
